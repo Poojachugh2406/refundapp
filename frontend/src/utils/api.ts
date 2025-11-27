243634fc2661cc04587e489d7203ee697d9617aa
rajatgupta2004 <rajatgupta562004@gmail.com>
2025-11-27 23:48:51 +0530

    adding correct be url

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -8,7 +8,7 @@ import type { CreateOrderData, Order, OrderWithDetails, UpdateOrderData, UpdateO
 import type { CreateRefundData, Refund, RefundWithDetails, UpdateRefundData, UpdateRefundStatusData } from '../types/refunds.ts';
 
 // const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5050/api';
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://hawkagency-portal-kushal.vercel.app.app/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://hawkagency-portal-sg5a.vercel.app/api';
 
 // Create axios instance
 const api = axios.create({
