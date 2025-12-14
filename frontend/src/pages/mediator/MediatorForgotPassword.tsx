bdc0c1ecda2cb0b3619f932e886c90aaa61f80c4
Kushal <hawkagency.info@gmail.com>
2025-12-14 15:38:16 +0530

    Update MediatorForgotPassword.tsx

--- frontend/src/pages/mediator/MediatorForgotPassword.tsx
+++ frontend/src/pages/mediator/MediatorForgotPassword.tsx
@@ -167,8 +167,8 @@ const MediatorForgotPassword: React.FC = () => {
           position: absolute;
           top: 0;
           left: 0;
-          // width: 100%;
-          // height: 100%;
+          width: 100%;
+          height: 100%;
           display: flex;
           justify-content: center;
           flex-direction: column;
